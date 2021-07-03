111219e250f390ba25d2dd7fbb2e451f58e2c80b
adnjoo <adnjoo@gmail.com>
2021-07-02 19:55:58 -0700

    like btn

--- react-1/index.js
+++ react-1/index.js
@@ -1,22 +1,25 @@
 'use strict';
 
+//step 1
 const e = React.createElement;
 
+//step 2 create a class
 class LikeButton extends React.Component {
-  constructor(props) {
+  //step 3 constructor and props
+    constructor(props) {
     super(props);
     this.state = { liked: false };
   }
-
+  //step 4 render is a React function
   render() {
     if (this.state.liked) {
       return 'You liked this.';
     }
-
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
+    //step 5 return is a function which returns the element
+    return (
+      <button onClick={() => this.setState({ liked: true })}>
+        Like
+      </button>    
     );
   }
 }
