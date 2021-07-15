274187e52ed6c4b84beeea337b1500a953f93e1c
Andrew Njoo <adnjoo@gmail.com>
2021-07-14 20:23:53 -0700

    add typed.js

--- portfolio-website-2021-v1/index.js
+++ portfolio-website-2021-v1/index.js
@@ -1,12 +1,18 @@
-
-let profile = document.getElementById('profile')
-profile.addEventListener('click', function(){
+let profile = document.getElementById("profile");
+profile.addEventListener("click", function () {
   // profile.src='./pic.jpeg'
-  if (profile.src.includes('andrew.svg')){
-    console.log('t')
-    profile.src='./pic.jpeg'
+  if (profile.src.includes("andrew.svg")) {
+    console.log("t");
+    profile.src = "./pic.jpeg";
+  } else {
+    profile.src = "./andrew.svg";
   }
-   else {
-    profile.src='./andrew.svg'
-  }
-})
+});
+
+
+var typed = new Typed('#typed',{
+  strings : ['Hi, ^2000 my name is drew.'],
+  typeSpeed : 10,
+  loop : false,
+  showCursor:true
+});
