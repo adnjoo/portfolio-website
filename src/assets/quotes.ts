2a7ec40a12b95152e65f2faac5ee16a62aaa617f
Andrew Njoo <adnjoo@gmail.com>
2023-02-01 19:01:13 -0800

    refactor: abstract out quotes into file

---
+++ src/assets/quotes.ts
@@ -0,0 +1,78 @@
+interface Quote {
+    quote: string;
+    translation: string;
+}
+
+export const quotes: Quote[] = [
+    {
+        quote: "Il moto è causa d'ogni vita",
+        translation: 'Motion is the cause of all life'
+    },
+    {
+        quote: 'La conoscenza di tutte le cose è possibile',
+        translation: 'The knowledge of all things is possible'
+    },
+    {
+        quote: "La passione dell'animo caccia via la lussuria",
+        translation: 'Passion of the soul chases away lust'
+    },
+    {
+        quote: 'Chi poco pensa molto erra',
+        translation: 'Those who think little, err'
+    },
+    {
+        quote: "Chi tempo ha e tempo aspetta, perde l'amico e danari non ha mai",
+        translation:
+            'He who has time and awaits time, has neither friends nor money'
+    },
+    {
+        quote: "L'arte non è mai finita, solo abbandonata",
+        translation: 'Art is never finished, only abandoned'
+    },
+    {
+        quote: 'Ogni nostra cognizione prencipia da sentimenti',
+        translation: 'Our every cognition begins with feelings'
+    },
+    {
+        quote: 'Amor ogni cosa vince',
+        translation: 'Love conquers all'
+    },
+    {
+        quote: "No' si volta chi a stella è fisso",
+        translation: 'He who is fixed to a star does not change his mind'
+    },
+    {
+        quote:
+            "Quelli che s'innamoran di pratica sanza scienzia son come 'l nocchier ch'entra in navilio senza timone o bussola, che mai ha certezza dove si vada",
+        translation:
+            'Those who fall in love with practice without science are like the helmsman who enters a ship without a rudder or compass, who is never sure where to go.'
+    },
+    {
+        quote: "Sempre la pratica dev'essere edificata sopra la buona teorica",
+        translation: 'Practice must always be built on good theory'
+    },
+    {
+        quote: 'La scienza è il capitano, e la pratica sono i soldati',
+        translation: 'Science is the captain, and practice is the soldiers'
+    },
+    {
+        quote: 'La vita bene spesa lunga è',
+        translation: 'A life well lived is long'
+    },
+    {
+        quote: "L'acqua è la forza motrice della natura",
+        translation: 'Water is the driving force of nature'
+    },
+    {
+        quote: "Sono rimasto colpito dall'urgenza di fare. Conoscere non è abbastanza; dobbiamo applicare. Essere disposti non è abbastanza; dobbiamo fare",
+        translation: 'I was struck by the urgency to do. Knowing is not enough; we must apply. Being willing is not enough; we must do'
+    },
+    {
+        quote: 'La semplicità è la sofisticazione suprema',
+        translation: 'Simplicity is the ultimate sophistication'
+    },
+    {
+        quote: 'Il tempo rimane abbastanza a lungo per coloro che lo utilizzano',
+        translation: 'Time stays long enough for those who use it'
+    }
+];
