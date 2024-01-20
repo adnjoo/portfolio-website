4d01ee7590c6e74abff4ee3e0bf37da0b9b3fa82
Andrew Njoo <adnjoo@gmail.com>
2024-01-20 09:38:48 -0800

    navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,64 @@
+const siteTitle = "drew's site";
+
+const sitePages = [
+  {
+    name: 'Portfolio',
+    href: '/',
+  },
+  {
+    name: 'Blog',
+    href: '/blog',
+  }
+]
+
+const Navbar = () => {
+  return (
+    <div className='navbar bg-base-100'>
+      <div className='navbar-start'>
+        <div className='dropdown'>
+          <div tabIndex={0} role='button' className='btn btn-ghost lg:hidden'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-5 w-5'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M4 6h16M4 12h8m-8 6h16'
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className='menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow'
+          >
+            {sitePages.map((page) => (
+              <li key={page.name}>
+                <a href={page.href}>{page.name}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <a className='btn btn-ghost text-xl'>{siteTitle}</a>
+      </div>
+      <div className='navbar-center hidden lg:flex'>
+        <ul className='menu menu-horizontal px-1'>
+          {sitePages.map((page) => (
+            <li key={page.name}>
+              <a href={page.href}>{page.name}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className='navbar-end'>
+        <a className='btn'>Button</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
