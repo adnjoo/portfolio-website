18f17fe9a39403cda3e80f47fe7418362af88583
Andrew Njoo <adnjoo@gmail.com>
2024-01-20 09:42:20 -0800

    react-icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { IoMenu } from 'react-icons/io5';
+
 const siteTitle = "drew's site";
 
 const sitePages = [
@@ -8,8 +10,8 @@ const sitePages = [
   {
     name: 'Blog',
     href: '/blog',
-  }
-]
+  },
+];
 
 const Navbar = () => {
   return (
@@ -17,20 +19,7 @@ const Navbar = () => {
       <div className='navbar-start'>
         <div className='dropdown'>
           <div tabIndex={0} role='button' className='btn btn-ghost lg:hidden'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-5 w-5'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M4 6h16M4 12h8m-8 6h16'
-              />
-            </svg>
+            <IoMenu size={24} />
           </div>
           <ul
             tabIndex={0}
@@ -43,7 +32,7 @@ const Navbar = () => {
             ))}
           </ul>
         </div>
-        <a className='btn btn-ghost text-xl'>{siteTitle}</a>
+        <a className='btn btn-ghost text-xl' href='/'>{siteTitle}</a>
       </div>
       <div className='navbar-center hidden lg:flex'>
         <ul className='menu menu-horizontal px-1'>
