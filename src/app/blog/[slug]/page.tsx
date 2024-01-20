6c02e91504a4315edf85234ae1c0a5ed65e80d62
Andrew Njoo <adnjoo@gmail.com>
2024-01-20 12:10:04 -0800

    types

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,46 +1,12 @@
-import { MDXRemote } from 'next-mdx-remote/rsc';
-import remarkGfm from 'remark-gfm';
-import { getPost } from '../page';
-import { Code } from 'bright';
-
-export const mdxComponents = {
-  pre: Code,
-  h2: (props: any) => <h2 className='text-2xl font-semibold' {...props} />,
-  h3: (props: any) => <h3 className='text-xl font-semibold' {...props} />,
-  p: (props: any) => <p className='my-4' {...props} />,
-  a: (props: any) => (
-    <a
-      className='link-primary'
-      href={props.href}
-      target='_blank'
-      rel='noopener noreferrer'
-    >
-      {props.children}
-    </a>
-  ),
-  hr: (props: any) => <hr className='my-4' {...props} />,
-};
-
-export function PostBody({ children }: { children: string }) {
-  return (
-    <MDXRemote
-      source={children}
-      options={{
-        mdxOptions: {
-          remarkPlugins: [remarkGfm],
-        },
-      }}
-      components={mdxComponents}
-    />
-  );
-}
+import { getPost } from '../functions';
+import { PostBody } from '../components/PostBody';
 
 export default async function PostPage({
   params,
 }: {
   params: { slug: string };
 }) {
-  const post = await getPost(params.slug);
+  const post:any = await getPost(params.slug);
 
   //   console.log(post);
   return (
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,33 +1,4 @@
-import matter from 'gray-matter';
-import fs from 'fs/promises';
-import { cache } from 'react';
-
-export const getPosts = cache(async () => {
-  const posts = await fs.readdir('./posts');
-
-  const postPromises = posts.map(async (post) => {
-    const postContent = await fs.readFile(`./posts/${post}`, 'utf-8');
-    const { data, content } = matter(postContent);
-    const id = post.replace(/\.mdx$/, '');
-
-    return {
-      ...data,
-      id,
-      body: content,
-    };
-  });
-
-  const sortedPosts = (await Promise.all(postPromises)).sort((a, b) => {
-    return b.date.localeCompare(a.date);
-  });
-
-  return sortedPosts;
-});
-
-export async function getPost(slug: string) {
-  const posts = await getPosts();
-  return posts.find((post) => post.id === slug);
-}
+import { getPosts } from "./functions";
 
 export default async function Blog() {
   const posts = await getPosts();
@@ -37,7 +8,7 @@ export default async function Blog() {
     <div className='mt-12 flex flex-col items-center justify-center'>
       <h1 className='mb-12 text-3xl sm:text-5xl'>Blog</h1>
       <ul className='flex flex-col gap-4'>
-        {posts.map((post) => (
+        {posts.map((post: any) => (
           <li key={post.id} className='flex flex-col'>
             <span>
               {new Date(post.date).toLocaleDateString('en-US', {
