7f40057903faa51c3a71391d93528e428119a0ae
Andrew Njoo <adnjoo@gmail.com>
2024-01-21 14:43:06 -0800

    trending repos

--- src/components/DataCard.tsx
+++ src/components/DataCard.tsx
@@ -2,6 +2,7 @@
 
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { BsGithub } from 'react-icons/bs';
 
 export const DataCard = () => {
   const [data, setData] = useState();
@@ -9,45 +10,39 @@ export const DataCard = () => {
   useEffect(() => {
     const fetchData = async () => {
       const response = await axios.get(
-        'https://api.github.com/users/adnjoo/repos'
+        '/api/trending-repos'
       );
-      const sortedData = response.data.sort((a, b) => new Date(b.pushed_at) - new Date(a.pushed_at));
-      const top3Data = sortedData.slice(0, 5);
-      setData(top3Data);
+      setData(response.data);
     };
 
     fetchData();
   }, []);
 
+  if (!data) {
+    return <div className='mt-12 md:mt-24'>Loading...</div>;
+  }
+
   return (
-    <div className='card mt-12 w-96 bg-base-100 shadow-xl md:mt-24'>
-      <div className='card-body'>
-        <h1 className='card-title mb-4 text-lg font-semibold'>
-          Latest activity from GitHub
+    <div className='card mt-12 w-96 bg-base-100 shadow-xl md:mt-24 p-4 max-h-[400px] overflow-auto'>
+        <h1 className='card-title mb-4 text-md'>
+          Trending 
+          <BsGithub className='inline' />
+          Repos
         </h1>
 
         {data &&
           data?.map((d: any) => (
             <div key={d.id} className='mb-4'>
               <a
-                href={d.html_url}
+                href={d.url}
                 target='_blank'
-                className='text-blue-500 hover:underline'
+                className='link hover:underline'
               >
                 {d.name}
               </a>{' '}
-              <div className='text-gray-500'>Issues: {d.open_issues}</div>
-              <div className='text-gray-500'>
-                🕒 Latest Push:{' '}
-                {new Date(d.pushed_at).toLocaleDateString('en-US', {
-                  year: '2-digit',
-                  month: 'numeric',
-                  day: 'numeric',
-                })}
-              </div>{' '}
+              <div className='text-sm'>{d.description}</div>
             </div>
           ))}
-      </div>
     </div>
   );
 };
