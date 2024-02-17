aac8d95624cedda877225207bdc39993170f9198
Andrew Njoo <adnjoo@gmail.com>
2024-02-17 11:45:57 -0800

    animated pfp (#40)
    * edit css
    * comment

--- src/components/ProfilePicture.tsx
+++ src/components/ProfilePicture.tsx
@@ -3,7 +3,7 @@ import { classNames } from '@/lib/utils';
 const ProfilePicture = ({ className = '' }: { className?: string }) => {
   return (
     <img
-      className={classNames('my-4 h-16 w-16 rounded-full', className)}
+      className={classNames('my-4 h-16 w-16 rounded-full pfp', className)}
       src='https://avatars.githubusercontent.com/u/22807629?v=4'
       alt='pfp'
     />
