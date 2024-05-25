6bfdbac70f3348ccbe88a3c0a3044db55bba6666
Andrew Njoo <adnjoo@gmail.com>
2024-05-25 07:43:34 -0700

    rm

--- README.md
+++
@@ -1,4 +0,0 @@
-🥞: 
-- next 14.x
-- tailwind 3.x
-- radix 3.x
--- src/app/api/hello/route.ts
+++
@@ -1,6 +0,0 @@
-import { type NextRequest } from 'next/server';
-
-export async function GET(request: NextRequest) {
-  const path = request.nextUrl.pathname;
-  return new Response('Hello' + path);
-}
