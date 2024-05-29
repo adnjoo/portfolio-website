2423eff7a63e4209483c8821d30001a7f65d1ba5
Andrew Njoo <adnjoo@gmail.com>
2024-05-28 22:20:17 -0700

    steal-laa

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,9 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
   darkMode: 'class',
   content: [
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
+    './posts/**/*.{mdx}',
   ],
   theme: {
     extend: {
