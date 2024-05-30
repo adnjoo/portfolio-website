090308781c1620f0b104fa6e295f7bd4a38b214d
Andrew Njoo <adnjoo@gmail.com>
2024-05-29 23:28:37 -0700

    &apos;

--- src/app/swipe/page.tsx
+++ src/app/swipe/page.tsx
@@ -19,7 +19,7 @@ export default async function Swipe() {
     <div className='container mx-auto mt-8 px-4'>
       <h1 className='mb-4 text-2xl font-bold'>Bookmarks</h1>
       <div className='mb-4'>
-      Inspired by reading <Link href='/blog/steal-laa' className='my-link'>Steal LAA</Link>, I've decided to keep a swipe file, to keep tabs on cool Internet stuff.
+      Inspired by reading <Link href='/blog/steal-laa' className='my-link'>Steal LAA</Link>, I&apos;ve decided to keep a swipe file, to keep tabs on cool Internet stuff.
       </div>
       <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
         {data.map((bookmark: any) => (
